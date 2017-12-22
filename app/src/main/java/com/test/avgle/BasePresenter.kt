358264ce8fbbdbd1658f559252fc6d0c024a1afe
7a6ac0 <tabaco7517@gmail.com>
2017-12-22 11:27:24 +0800

    新增BaseView跟BasePresenter

---
+++ app/src/main/java/com/test/avgle/BasePresenter.kt
@@ -0,0 +1,10 @@
+package com.test.avgle
+
+/**
+ * Created by admin on 2017/12/22.
+ */
+interface BasePresenter {
+
+    fun start()
+
+}
---
+++ app/src/main/java/com/test/avgle/BaseView.kt
@@ -0,0 +1,10 @@
+package com.test.avgle
+
+/**
+ * Created by admin on 2017/12/22.
+ */
+interface BaseView<T> {
+
+    var presenter: T
+
+}
